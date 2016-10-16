f03fb506080ac30e91b3c8bfcb5bd48145772226
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-10-16 14:01:21 +0300

    add: loading users from server in package mode

--- src/processing/loaders/UserLoaderService.java
+++ src/processing/loaders/UserLoaderService.java
@@ -2,71 +2,107 @@ package processing.loaders;
 
 import com.google.gson.Gson;
 import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
 import processing.data.User;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserLoaderService extends DataLoaderService<User> {
-    @NotNull private final Deque<Integer> usersToProcess;
-    @NotNull private final Set<Integer> processedUsers;
+    private static final Logger log = Logger.getLogger(UserLoaderService.class.getCanonicalName());
 
     public UserLoaderService(@NotNull Consumer<User> dataProcessor) {
         super(dataProcessor, new UserLoader());
-        usersToProcess = new ArrayDeque<>();
-        processedUsers = new HashSet<>();
     }
 
     @Override
     public void loadData(int startId, int endId) {
-        try {
-            for (int id = startId; id <= endId; ) {
-                User[] users = dataLoader.loadPage(id);
+        for (int id = startId; id <= endId; ) {
+            User[] users = dataLoader.loadPage(id);
+            if (users != null) {
                 for (User user : users) {
                     dataProcessor.accept(user);
                 }
-                id += users.length;
+                id = users[users.length - 1].getId();
+            } else {
+                log.log(Level.WARNING, "Failed to get users from page since id = " + id);
             }
-        } catch (Exception ignored) {
         }
     }
 
     private static class UserLoader extends DataLoader<User> {
-        @NotNull private final String requestURL = "https://api.github.com/users/";
-        @NotNull private final String requestType = "GET";
+        @NotNull private final String GITHUB_DEFAULT_URL = "https://api.github.com";
+        @NotNull private String defaultRequestUrl = GITHUB_DEFAULT_URL + "/users";
+        @NotNull private final Gson gson = new Gson();
 
+        /**
+         * Load pack of users data, starting from @id.
+         * Size of page approximately about 45 users
+         */
+        @Nullable
         @Override
         protected User[] loadPage(int id) {
             try {
-                URL url = new URL(createRequestUrl(id));
-                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-                connection.setRequestMethod(requestType);
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                    String readLine;
+                try (BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(getConnectionDataStream(id)))) {
+                    String currentLine;
                     StringBuilder result = new StringBuilder();
-                    while ((readLine = reader.readLine()) != null) {
-                        result = result.append(readLine);
+                    while ((currentLine = reader.readLine()) != null) {
+                        result = result.append(currentLine);
                     }
-                    System.out.println("Read answer for id " + id + "\"" + result + "\"");
-                    return new Gson().fromJson(result.toString(), User[].class);
+                    return gson.fromJson(result.toString(), User[].class);
                 }
             } catch (IOException | RuntimeException e) {
                 e.printStackTrace();
+                log.log(Level.SEVERE, e.getMessage(), e.getLocalizedMessage());
             }
             return null;
         }
 
         @NotNull
-        String createRequestUrl(int id) {
-            return requestURL + "shx7";
+        private InputStream getConnectionDataStream(int id) throws IOException {
+            String currentRequestUrl = defaultRequestUrl;
+            HttpsURLConnection connection = null;
+            Map<String, List<String>> headerFields = null;
+
+            while (!isRequestSucceded(headerFields)) {
+                URL url = new URL(currentRequestUrl + "?since=" + id);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                headerFields = connection.getHeaderFields();
+                currentRequestUrl = getUpdatedRequestUrl(headerFields, currentRequestUrl);
+            }
+
+            return connection.getInputStream();
+        }
+
+        private boolean isRequestSucceded(@Nullable Map<String, List<String>> responseHeaders) {
+            if (responseHeaders != null) {
+                List<String> value = responseHeaders.get("Status");
+                if (value != null) {
+                    if (value.contains("200 OK")) return true;
+                }
+            }
+            return false;
+        }
+
+        @NotNull
+        private String getUpdatedRequestUrl(@NotNull Map<String, List<String>> headersFields,
+                                            @NotNull String oldRequestUrl) {
+            List<String> redirectionUrl = headersFields.get("Location");
+            if (redirectionUrl != null) {
+                return redirectionUrl.get(0);
+            }
+            return oldRequestUrl;
         }
     }
 }
