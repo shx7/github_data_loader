6542c7008e634fd0f3683e312bc607de895aad6e
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-10-16 20:18:43 +0300

    add: flushing recieved data in FIleUsersConsumer

--- src/processing/loaders/users/FileUsersConsumer.java
+++ src/processing/loaders/users/FileUsersConsumer.java
@@ -11,10 +11,15 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileUsersConsumer implements Consumer<User> {
+    private static final Logger log = Logger.getLogger(FileUsersConsumer.class.getCanonicalName());
+    private static final long FLUSH_THRESHOLD = 200;
     private int startId = -1;
     private int endId = -1;
+    private int count = 0;
     @NotNull private final List<User> data;
 
     public FileUsersConsumer() {
@@ -28,11 +33,26 @@ public class FileUsersConsumer implements Consumer<User> {
         }
         endId = user.getId();
         data.add(user);
+        ++count;
+        checkThreshold();
+    }
+
+    private void checkThreshold() {
+        if (count >= FLUSH_THRESHOLD) {
+            try {
+                flush();
+            } catch (IOException e) {
+                log.log(Level.WARNING, e.getLocalizedMessage());
+            }
+        }
     }
 
     public void flush() throws IOException {
         Path path = FileUtil.getUsersFile(startId, endId);
         PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path));
         data.forEach(user -> writer.write(user.toString()));
+        startId = endId;
+        count = 0;
+        data.clear();
     }
 }
