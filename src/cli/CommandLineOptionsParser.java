2f3e411c323023c943e79f74d4668796d996825a
Lyblinskii Alexander <lyblinskii.shx@gmail.com>
2016-11-05 21:28:24 +0300

    add: initial cli

--- src/cli/CommandLineOptionsParser.java
+++ src/cli/CommandLineOptionsParser.java
@@ -1,14 +1,31 @@
 package cli;
 
 import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.logging.Logger;
+import org.apache.commons.cli.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 public class CommandLineOptionsParser {
+    private static final Logger log = Logger.getLogger(CommandLineOptionsParser.class);
 
     @NotNull
-    public Map<String, String> parseCommandLineInput(@NotNull String[] commandLineArguments) {
-        return new HashMap<>();
+    public Map<String, Object> parseCommandLineInput(@NotNull String[] commandLineArguments) {
+        Map<String, Object> result = new HashMap<>();
+        Options options = new Options();
+        options.addOption("mode", true, "Select which structures to download: users, repos, orgs")
+               .addOption("start_id", true, "From which id start downloading")
+               .addOption("end_id", true, "End of downloading id range");
+        Parser parser = new PosixParser();
+        try {
+            for (Option option : parser.parse(options, commandLineArguments).getOptions()) {
+                result.put(option.getArgName(), option.getValue());
+            }
+        } catch (ParseException e) {
+            log.log(Level.WARNING, e.getMessage());
+        }
+        return result;
     }
 }
